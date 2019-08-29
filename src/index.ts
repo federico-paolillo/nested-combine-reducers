1e484e0e49eff2f7240abd225e25b9a6eebf4d23
Federico Paolillo <federico_paolillo@hugoboss.com>
2019-08-29 14:35:38 +0200

    Completely rewrote recursive implementation to be iterative
    This commit rewrotes completely the nested combine reducers implementation to use loops.
    The new implementation manually manages the stack to simulate a recursive implementation without actually being recursive.
    It is possible that this implementation introduces performance penalties as it uses more memory to manage the manual stack.
    Despite introducing support for very very deep object maps the chance to encounter such reducer maps is nearly impossible.
    
    Nontheless I can finally rejoice, for I have created my approximation of infinite.

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,53 @@
+import { Reducer, Action, NestedReducersMap, CombineReducersFn } from "./types";
+
 /**
- * The simplest definition of an Action
+ * The the element that will be returned when calling .pop() without removing it
  */
-export interface Action<T = any> {
-    type: T;
+function peek<TElement>(stack: TElement[]): TElement {
+
+    return stack[stack.length - 1];
+
 }
-
-/**
- * An object whose properties are reducing functions.
- */
-export type ReducersMap<S = any, A extends Action = Action> = {
-    [prop in keyof S]: Reducer<S[prop], A>;
-}
-
-/**
- * An object whose properties are either reducing function or @see NestedReducersMap
- */
-export type NestedReducersMap<S = any, A extends Action = Action> = {
-    [prop in keyof S]: Reducer<S[prop], A> | NestedReducersMap<S[prop], A>
-}
-
-/**
- * A reducing function
- */
-export type Reducer<S = any, A extends Action = Action> = (state: S | undefined, action: A) => S;
-
-/**
- * A function that turns an object whose properties are reducing functions into a single reducing function.
- */
-export type CombineReducersFn<S = any, A extends Action = Action> = (reducersMap: ReducersMap<S, A>) => Reducer<S, A>
 
 /**
  * Execution data recorded to resume a partial execution later on
  */
 interface StackFrame {
 
+    /**
+     * Input reducers map
+     */
     map: object;
 
-    mapKeys: string[];
-
-    currentMapKey: string | null;
-
-    currentMapKeyIndex: number;
-
-    previousMapKey: string | null;
-
+    /**
+     * Output flattened reducers map that will be combined
+     */
     flatMap: any;
 
-    previousFlatMap: any;
+    /**
+     * Loop variable, keeps track of which index we are currently on
+     */
+    currentMapKeyIndex: number;
+
+    /**
+     * We don't have a way to suspend and resume execution to a particular line.
+     * When an inner function call must return its value back to the caller we store where we should put the result.
+     */
+    previousFrameMapKey: string | null;
+
+    /**
+    * We don't have a way to suspend and resume execution to a particular line.
+    * When an inner function call must return its value back to the caller we store the object where we will put the result.
+    */
+    previousFrameFlatMap: any;
+
+    /**
+     * Flag, when true the function call this Stack Frame represents was not completed.
+     * When false the function was runned completely and we can execute the function return statement logic.
+     */
+    suspended: boolean;
 
 }
-
-/**
- * Peeks an element from the Stack given.
- * Peeking means getting the element that you would get with a pop() without actually removing the element from the Stack.
- */
-// function peek<TElement>(stack: TElement[]): TElement {
-
-//     if (!Array.isArray(stack)) throw new Error('The array-thing passed is not really an Array !');
-//     if (stack.length === 0) throw new Error('You cant peek a Stack that has no elements !');
-
-//     return stack[stack.length - 1];
-// }
 
 /**
  * Takes a Reducers maps with multiple levels of nesting and turns it into in a single reducing function.
@@ -79,16 +66,16 @@ export function nestedCombineReducers<S = any, A extends Action = Action>(
     const finalFlatMap: any = {};
 
     //Initialize manual Stack with initial values
+    //The initial frame will use the flatMap declared here as it will be the final result container
 
     const manualStack: StackFrame[] = [
         {
             map,
-            mapKeys: [],
-            currentMapKey: null,
             currentMapKeyIndex: 0,
             flatMap: finalFlatMap,
-            previousMapKey: null,
-            previousFlatMap: {}
+            previousFrameMapKey: null,
+            previousFrameFlatMap: {},
+            suspended: false
         }
     ];
 
@@ -96,20 +83,19 @@ export function nestedCombineReducers<S = any, A extends Action = Action>(
 
     while (manualStack.length > 0) {
 
-        const currentStackFrame = manualStack.pop();
+        const currentStackFrame = peek(manualStack);
 
-        if (typeof currentStackFrame === 'undefined') throw new Error('Got an undefined Stack Frame !');
+        currentStackFrame.suspended = false;
 
-        currentStackFrame.mapKeys = Object.keys(currentStackFrame.map);
+        const currentMapKeys = Object.keys(currentStackFrame.map);
 
-        for (let index = currentStackFrame.currentMapKeyIndex; index < currentStackFrame.mapKeys.length; index++) {
+        //Every frame resume starts after the previous loop of the cycle
 
-            currentStackFrame.currentMapKey = currentStackFrame.mapKeys[index];
-            currentStackFrame.currentMapKeyIndex = index;
+        for (let index = currentStackFrame.currentMapKeyIndex; index < currentMapKeys.length; index++) {
 
-            console.log(index);
+            const currentMapKey = currentMapKeys[index];
 
-            const propValue = map[currentStackFrame.currentMapKey];
+            const propValue = currentStackFrame.map[currentMapKey];
 
             if (propValue === null) continue;
             if (typeof propValue === 'undefined') continue;
@@ -117,7 +103,7 @@ export function nestedCombineReducers<S = any, A extends Action = Action>(
             //Hopefully a reducer function, let's store it to combine it later
 
             if (typeof propValue === 'function') {
-                currentStackFrame.flatMap[currentStackFrame.currentMapKey] = propValue;
+                currentStackFrame.flatMap[currentMapKey] = propValue;
             }
 
             //Nesting found, let's go deeper !
@@ -128,19 +114,20 @@ export function nestedCombineReducers<S = any, A extends Action = Action>(
 
                 const nextStackFrame: StackFrame = {
                     map: propValue,
-                    mapKeys: [],
-                    currentMapKey: null,
                     currentMapKeyIndex: 0,
                     flatMap: {},
-                    previousMapKey: currentStackFrame.currentMapKey,
-                    previousFlatMap: currentStackFrame.flatMap
+                    previousFrameMapKey: currentMapKey,
+                    previousFrameFlatMap: currentStackFrame.flatMap,
+                    suspended: false
                 };
 
-                //Keep this frame on the Stack as it has not finished yet
-                
-                manualStack.push(currentStackFrame);
+                //Mark this frame on the Stack as suspended, because to complete its execution it must wai previous frames
+                //When resuming we will skip right to the next cycle of the loop
 
-                //But first run this frame
+                currentStackFrame.suspended = true;
+                currentStackFrame.currentMapKeyIndex = index + 1;
+
+                //Then run this new frame and interrupt the execution of the current one
 
                 manualStack.push(nextStackFrame);
 
@@ -151,11 +138,20 @@ export function nestedCombineReducers<S = any, A extends Action = Action>(
         }
 
         //If we have a previousMapKey it means that we left a previous execution incomplete
-        //We have to update previousMapKey location so that the previous execution can resume back with the result
+        //We have to update previousMapKey location so that the previous execution can resume back with some results
+        //We can only do that if this frame is not suspdended, if it is suspended it means that is waiting for some data
 
-        if (currentStackFrame.previousMapKey) {
+        if (currentStackFrame.previousFrameMapKey && !currentStackFrame.suspended) {
 
-            currentStackFrame.previousFlatMap[currentStackFrame.previousMapKey] = combineReducersFn(currentStackFrame.flatMap);
+            currentStackFrame.previousFrameFlatMap[currentStackFrame.previousFrameMapKey] = combineReducersFn(currentStackFrame.flatMap);
+
+        }
+
+        //If this frame is not suspended we can destroy it because its execution is complete
+
+        if (!currentStackFrame.suspended) {
+
+            manualStack.pop();
 
         }
 
